b2d308e626db4f16616dd6eb0799c85b04c28782
davut <dkilinc@hotmail.com>
2014-04-04 08:19:09 +0300

    d

--- RestKit-development/Examples/RKTwitter/Classes/index/DataModels.h
+++
@@ -1,8 +0,0 @@
-//
-//  DataModels.h
-//
-//  Created by   on 04/02/14
-//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
-//
-
-#import "ListPrice.h"#import "Groups.h"#import "Action.h"#import "Ads.h"#import "baseIndexModel.h"#import "StrikeoutPrice.h"#import "Installment.h"#import "Items.h"
--- SponsorPayJson/DataModels/DataModels.h
+++
@@ -1,8 +0,0 @@
-//
-//  DataModels.h
-//
-//  Created by davut kilinc on 3.04.2014
-//  Copyright (c) 2014 tmob. All rights reserved.
-//
-
-#import "SponsorPayInformation.h"#import "SponsorPayOfferTypes.h"#import "SponsorPayTimeToPayout.h"#import "SponsorPayBaseClass.h"#import "SponsorPayThumbnail.h"#import "SponsorPayOffers.h"
